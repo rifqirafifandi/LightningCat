3dfc30f6befedb0d5f607fae65416a12cf2f824c
dangkhoasdc <letan.dangkhoa@gmail.com>
2025-02-25 21:03:57 +0800

    script to insert facility to db

--- recommender/scripts/insert_facilities.py
+++ recommender/scripts/insert_facilities.py
@@ -2,13 +2,19 @@
 Insert facilities data to mongodb
 """
 import typer
+import json
 from recommender import database
 from pathlib import Path
 
 def main(
     input_path: Path = typer.Argument(..., file_okay=True, exists=True, readable=True, help="Path to  SportSG Sport Facilities geojson file")
 ):
-    ...
+    with open(input_path, "r") as f:
+        data = json.load(f)
+
+    db = database.DB()
+    db.facility.insert_many(data)
+    print("Done !!")
 
 
 if __name__ == "__main__":
