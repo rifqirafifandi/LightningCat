965f1c997277957104dc710b98b2914a2fd73c49
dangkhoasdc <letan.dangkhoa@gmail.com>
2025-02-25 23:11:00 +0800

    formatter

--- recommender/tests/mongodb_test.py
+++ recommender/tests/mongodb_test.py
@@ -11,10 +11,10 @@ MONGODB_PWD = os.getenv("MONGODB_PWD")
 
 uri = f"mongodb+srv://{MONGODB_USER}:{MONGODB_PWD}@lightning-cat.0i8os.mongodb.net/?retryWrites=true&w=majority&appName=lightning-cat"
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri, server_api=ServerApi("1"))
 # Send a ping to confirm a successful connection
 try:
-    client.admin.command('ping')
+    client.admin.command("ping")
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
