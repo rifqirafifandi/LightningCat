100e56901a216fbaf7d8aaa841f9c34131e890d9
dangkhoasdc <letan.dangkhoa@gmail.com>
2025-03-02 18:54:24 +0800

    update db config

--- training-pipeline/src/training_pipeline/database.py
+++ training-pipeline/src/training_pipeline/database.py
@@ -17,8 +17,10 @@ def get_db_client():
 
     MONGODB_USER = os.getenv("MONGODB_USER")
     MONGODB_PWD = os.getenv("MONGODB_PWD")
+    MONGODB_ADDRESS = os.getenv("MONGODB_ADDRESS")
+    MONGODB_APP = os.getenv("MONGODB_APP")
 
-    uri = f"mongodb+srv://{MONGODB_USER}:{MONGODB_PWD}@lightning-cat.0i8os.mongodb.net/?retryWrites=true&w=majority&appName=lightning-cat"
+    uri = f"mongodb+srv://{MONGODB_USER}:{MONGODB_PWD}@{MONGODB_ADDRESS}/?retryWrites=true&w=majority&appName={MONGODB_APP}"
 
     client = MongoClient(uri, server_api=ServerApi("1"))
     return client
