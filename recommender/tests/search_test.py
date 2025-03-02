f590302b6d1968123e6c06f5c32fb81f0fe639ab
dangkhoasdc <letan.dangkhoa@gmail.com>
2025-03-02 18:52:57 +0800

    search facility via vector database

--- recommender/tests/search_test.py
+++ recommender/tests/search_test.py
@@ -1,3 +1,4 @@
+from pprint import pp
 from pathlib import Path
 from recommender.inference import Searcher
 
@@ -6,4 +7,9 @@ INDEX_PATH = Path("output_index/")
 searcher = Searcher.from_local_path(INDEX_PATH)
 
 # case 1: user name search
-searcher.search("f0a9d68f-71d3-4d75-abd9-6aea81b5066b")
+res = searcher.search_user("f0a9d68f-71d3-4d75-abd9-6aea81b5066b")
+pp(res)
+
+res = searcher.search_facilities_from_user("f0a9d68f-71d3-4d75-abd9-6aea81b5066b", top_k=5)
+for f in res:
+    print(f"name={f['name']}, location={f['location']}")
