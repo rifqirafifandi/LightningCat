3e94c30270bd146f380d9dd212cbb30f94894a99
fm <laifoongming@gmail.com>
2025-03-12 17:14:25 +0800

    more legacy code cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,12 @@ import Navbar from './components/Navbar';
 import FilterPanel from './components/FilterPanel';
 import SidePanel from './components/SidePanel';
 import SearchInputBox from './components/SearchInputBox';
-
 import Queries from './queries/Queries';
 import { useLazyQuery } from '@apollo/client';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 const App = () => {
-  // initialise state and refs
   const mapContainerRef = useRef(null);
   const [map, setMap] = useState(null);
   const [areaState, setAreaState] = useState({
@@ -26,34 +24,12 @@ const App = () => {
     minSqf: undefined,
     maxSqf: undefined,
   });
-
-  // New state to hold the custom record data from a polygon click
+  
   const [customRecordsData, setCustomRecordsData] = useState(null);
-  const [getRecordsData, { 
-    error: recordsError,
-    loading: recordsLoading,
-    data: recordsData 
-  }] = useLazyQuery(Queries.GET_RECORDS, { variables: areaState });
-  const [getListingsData, { 
-    error: listingsError, 
-    loading: listingsLoading, 
-    data: listingsData 
-  }] = useLazyQuery(Queries.GET_LISTINGS, { variables: areaState });
-  const [getDistinctTowns, { 
-    error: townsError, 
-    loading: townsLoading, 
-    data: townsData 
-  }] = useLazyQuery(Queries.GET_DISTINCT_TOWNS);
-  const [getDistinctFlatTypes, { 
-    error: flatTypesError, 
-    loading: flatTypesLoading, 
-    data: flatTypesData 
-  }] = useLazyQuery(Queries.GET_DISTINCT_FLAT_TYPES);
-  const [getRecordsAvgPrice, { 
-    error: avgPriceError, 
-    loading: avgPriceLoading, 
-    data: avgPriceData 
-  }] = useLazyQuery(Queries.GET_RECORDS_AVG_PRICE, { variables: areaState });
+  
+  const [getListingsData, { error: listingsError, loading: listingsLoading, data: listingsData }] = useLazyQuery(Queries.GET_LISTINGS, { variables: areaState });
+  const [getDistinctTowns, { data: townsData }] = useLazyQuery(Queries.GET_DISTINCT_TOWNS);
+  const [getDistinctFlatTypes, { data: flatTypesData }] = useLazyQuery(Queries.GET_DISTINCT_FLAT_TYPES);
 
   useEffect(() => {
     const mapInstance = new mapboxgl.Map({
@@ -65,110 +41,76 @@ const App = () => {
 
     mapInstance.on('load', () => {
       updateBounds(mapInstance.getBounds().toArray());
-      getRecordsAvgPrice();
-      getRecordsData();
       getListingsData();
       getDistinctTowns();
       getDistinctFlatTypes();
 
-      // Load and display polygons from the preprocessed GeoJSON file
       fetch('/SportSGSportFacilitiesGEOJSON.geojson')
         .then(response => response.json())
         .then(data => {
-          mapInstance.addSource('sportFacilities', {
-            type: 'geojson',
-            data: data
-          });
-
+          mapInstance.addSource('sportFacilities', { type: 'geojson', data });
+          
           mapInstance.addLayer({
             id: 'sportFacilitiesFill',
             type: 'fill',
             source: 'sportFacilities',
-            layout: {},
-            paint: {
-              'fill-color': '#800080',
-              'fill-opacity': 0.5
-            }
+            paint: { 'fill-color': '#800080', 'fill-opacity': 0.5 }
           });
-
+          
           mapInstance.addLayer({
             id: 'sportFacilitiesBorder',
             type: 'line',
             source: 'sportFacilities',
-            layout: {},
-            paint: {
-              'line-color': '#000000',
-              'line-width': 2
-            }
+            paint: { 'line-color': '#000000', 'line-width': 2 }
           });
 
           mapInstance.on('mouseenter', 'sportFacilitiesFill', () => {
             mapInstance.getCanvas().style.cursor = 'pointer';
           });
+          
           mapInstance.on('mouseleave', 'sportFacilitiesFill', () => {
             mapInstance.getCanvas().style.cursor = '';
           });
 
-          // Inside your mapInstance.on('click', 'sportFacilitiesFill', ...) handler:
           mapInstance.on('click', 'sportFacilitiesFill', (e) => {
             const feature = e.features && e.features[0];
             if (feature) {
-              let popupContent = '<table>';
               let recordObj = {};
               Object.entries(feature.properties).forEach(([key, value]) => {
-                if (key === 'Description' || value === null || value === '') return;
-                popupContent += `<tr><th>${key}</th><td>${value}</td></tr>`;
-                recordObj[key] = value;
+                if (key !== 'Description' && value) {
+                  recordObj[key] = value;
+                }
               });
-              popupContent += '</table>';
               
               new mapboxgl.Popup({ maxWidth: "600px" })
                 .setLngLat(e.lngLat)
-                .setHTML(popupContent)
+                .setHTML(`<table>${Object.entries(recordObj).map(([key, val]) => `<tr><th>${key}</th><td>${val}</td></tr>`).join('')}</table>`)
                 .addTo(mapInstance);
-
-              // Set custom records data with a flag to indicate custom popup data
+              
               setCustomRecordsData({ isCustom: true, getRecords: [recordObj] });
             }
           });
         })
-        .catch(err => {
-          console.error('Error loading sport facilities GeoJSON:', err);
-        });
+        .catch(err => console.error('Error loading sport facilities GeoJSON:', err));
     });
 
     mapInstance.on('moveend', () => {
       updateBounds(mapInstance.getBounds().toArray());
-      getRecordsAvgPrice();
-      getRecordsData();
       getListingsData();
     });
 
     setMap(mapInstance);
-
-    return () => {
-      mapInstance.remove();
-    };
-  }, [getRecordsData, getListingsData, getRecordsAvgPrice]);
+    return () => mapInstance.remove();
+  }, [getListingsData]);
 
   const updateBounds = (bounds) => {
     const [lonMin, latMin, lonMax, latMax] = [bounds[0][0], bounds[0][1], bounds[1][0], bounds[1][1]];
-    setAreaState(prevState => ({
-      ...prevState,
-      latRangeGte: latMin,
-      latRangeLte: latMax,
-      lonRangeGte: lonMin,
-      lonRangeLte: lonMax
-    }));
+    setAreaState(prevState => ({ ...prevState, latRangeGte: latMin, latRangeLte: latMax, lonRangeGte: lonMin, lonRangeLte: lonMax }));
   };
 
   const onFlyTo = ({ lng, lat }) => {
     if (map) {
-      map.flyTo({
-        center: [lng, lat],
-        essential: true,
-        zoom: 15,
-      });
+      map.flyTo({ center: [lng, lat], essential: true, zoom: 15 });
     }
   };
 
@@ -179,9 +121,7 @@ const App = () => {
         <div className="row">
           <div className="col-md-6">
             <div className="row mt-1 mb-1">
-              <div className="col-md-6">
-                {map && <SearchInputBox onFlyTo={onFlyTo} />}
-              </div>
+              <div className="col-md-6">{map && <SearchInputBox onFlyTo={onFlyTo} />}</div>
               <div className="col-md-6">
                 <FilterPanel areaState={areaState} setAreaState={setAreaState} towns={townsData} flatTypes={flatTypesData} />
               </div>
@@ -190,16 +130,12 @@ const App = () => {
           </div>
           <div className="col-md-6">
             <SidePanel 
-              recordsLoading={recordsLoading}
               listingsLoading={listingsLoading}
-              avgPriceLoading={avgPriceLoading}
-              recordsData={customRecordsData ? customRecordsData : recordsData}
+              recordsData={customRecordsData}
               listingsData={listingsData}
-              avgPriceData={avgPriceData}
             />
           </div>
         </div>
-
       </div>
     </div>
   );
