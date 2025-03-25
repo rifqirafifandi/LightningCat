b852f2529a714d5a6278e35680ccf6b58e7a768b
dangkhoasdc <letan.dangkhoa@gmail.com>
2025-03-26 00:55:06 +0800

    add middleware to fix CORS issue

--- recommender/src/recommender/app.py
+++ recommender/src/recommender/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from path import Path
 
 from recommender.inference import Searcher
@@ -11,6 +12,15 @@ ARTIFACT_PATH = Path("./data/")
 
 
 app = FastAPI(debug=True)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
 real_db = LocalRealTimeDB("./data/facilityCapacities_sample.json")
 
 search_app: Searcher = Searcher.from_local_path(MODEL_PATH, ARTIFACT_PATH)
