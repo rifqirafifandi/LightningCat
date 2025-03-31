1f8c6428c507be52bcc971d83f22f995013d8c54
Edison Chee <edisonchee@live.com>
2025-04-01 00:17:55 +0800

    feat: frontend empty states, enum types for creating listings and bookings

--- frontend/src/pages/Account/Bookings/Bookings.jsx
+++ frontend/src/pages/Account/Bookings/Bookings.jsx
@@ -1,7 +1,73 @@
+import React from 'react';
+import { Button, Table } from 'react-bootstrap';
+import { useAuth } from '../../../contexts/auth';
+import styles from './Bookings.module.css';
+import EmptyStateGenericImage from '../../../assets/images/empty-state-generic.svg';
+
 const Bookings = () => {
+  const { user } = useAuth();
+  const [bookings, setBookings] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://api.chucklenuts.party/bookings/${user.id}`, {
+          method: 'GET',
+          credentials: 'include',
+        });
+
+        if (response.status > 204) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        setBookings(data);
+      } catch (error) {
+        console.error('Error fetching listings:', error);
+      }
+    };
+
+    fetchData();
+  });
+
   return (
     <div>
+      <div className={styles.headerContainer}>
       <h1>My Bookings</h1>
+      <Button>Make bookings</Button>
+      </div>
+      {
+        bookings.length
+        ? <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Listing ID</th>
+                <th>Booking status</th>
+                <th>Amount</th>
+                <th>Payment status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                bookings.map((booking) => (
+                  <tr>
+                    <td>{ booking.id }</td>
+                    <td>{ booking.listing_id }</td>
+                    <td>{ booking.booking_status }</td>
+                    <td>{ booking.amount }</td>
+                    <td>{ booking.payment_status }</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </Table>
+        : <div className={styles.emptyStateContainer}>
+            <img src={EmptyStateGenericImage} alt="No bookings" className={styles.emptyStateImage} />
+            <p>No bookings found.</p>
+          </div>
+      }
+      
     </div>
   );
 }
--- frontend/src/pages/Account/Listings/Listings.jsx
+++ frontend/src/pages/Account/Listings/Listings.jsx
@@ -1,7 +1,76 @@
+import React from 'react';
+import { Button, Table } from 'react-bootstrap';
+import { useAuth } from '../../../contexts/auth';
+import styles from './Listings.module.css';
+import EmptyStateGenericImage from '../../../assets/images/empty-state-generic.svg';
+
 const Listings = () => {
+  const { user } = useAuth();
+  const [listings, setListings] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://api.chucklenuts.party/listings/${user.id}`, {
+          method: 'GET',
+          credentials: 'include',
+        });
+
+        if (response.status > 204) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        setListings(data);
+      } catch (error) {
+        console.error('Error fetching listings:', error);
+      }
+    };
+
+    fetchData();
+  });
+
   return (
     <div>
+      <div className={styles.headerContainer}>
       <h1>My Listings</h1>
+      <Button>Post listing</Button>
+      </div>
+      {
+        listings.length
+        ? <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Facility name</th>
+                <th>Activity</th>
+                <th>Time</th>
+                <th>Capacity</th>
+                <th>Price</th>
+                <th>status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                listings.map((listing) => (
+                  <tr>
+                    <td>{ listing.id }</td>
+                    <td>{ listing.facility_name }</td>
+                    <td>{ listing.activity }</td>
+                    <td>{ listing.start_time } - { listing.end_time }</td>
+                    <td>{ listing.capacity }</td>
+                    <td>{ listing.price }</td>
+                    <td>{ listing.status }</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </Table>
+        : <div className={styles.emptyStateContainer}>
+            <img src={EmptyStateGenericImage} alt="No listings" className={styles.emptyStateImage} />
+            <p>You have no listings.</p>
+          </div>
+      }
     </div>
   );
 }
